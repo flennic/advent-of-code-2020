7606169f5ad6d2fa6e4d7964dbffd4df7c4d5dc9
Maximilian Pfundstein <maximilian@pfundstein.org>
2020-12-02 22:40:59 +0100

    Add day 2

--- .idea/sonarlint/issuestore/index.pb
+++ .idea/sonarlint/issuestore/index.pb
@@ -1,3 +1,7 @@
 
-7
-1/1.kts,a/2/a2d7c6fb8b249dfa84c3e5d4df34cb64a54b2aa2
+R
+"advent-of-code-day-1/src/input.txt,f/f/ffd78c8c90a812857e7caedd10a82a6d0c331d81
+Q
+!advent-of-code-day-1/src/partA.kt,b/8/b889aa6b37870043fb54cfcb98f2812cf8abd182
+Q
+!advent-of-code-day-1/src/partB.kt,d/7/d7e0f7a42874bfe12a4161c48754375dbe70bf87
---
+++ advent-of-code-day-2/src/partA.kt
@@ -0,0 +1,78 @@
+/**
+ * Created by Maximilian Pfundstein on 02/12/2020.
+ */
+
+
+import java.io.File
+import java.io.InputStream
+
+/**
+ * Created by Maximilian Pfundstein on 01/12/2020.
+ */
+
+
+fun main() {
+
+    val inputStream: InputStream = File("advent-of-code-day-2/src/input.txt").inputStream()
+    val lineList = mutableListOf<String>()
+
+    inputStream.bufferedReader().useLines { lines -> lines.forEach {
+        if (it.isNotEmpty())
+            lineList.add(it)
+    }}
+
+    val noCorrectPasswords = lineList.map { isPasswordValid(getMetrics(it)) }.toList().sumBy { if (it) 1 else 0 }
+    val noCorrectPasswords2 = lineList.map { isPasswordValid2(getMetrics(it)) }.toList().sumBy { if (it) 1 else 0 }
+
+    println(noCorrectPasswords)
+    println(noCorrectPasswords2)
+}
+
+fun isPasswordValid2(dict: Map<String, String>): Boolean {
+
+    var positionOne = dict["first"]?.toInt() ?: return false
+    var positionTwo = dict["second"]?.toInt() ?: return false
+    val password = dict["password"].toString()
+
+    positionOne--
+    positionTwo--
+
+    val policyChar: Char = dict["character"]?.get(0) ?: return false
+
+    val matchOne = password[positionOne] == policyChar
+    val matchTwo = password[positionTwo] == policyChar
+
+    return matchOne xor matchTwo
+}
+
+fun isPasswordValid(dict: Map<String, String>): Boolean {
+
+    var occurrence = 0
+    val lowerRange = dict["first"]?.toInt() ?: return false
+    val upperRange = dict["second"]?.toInt() ?: return false
+
+    val policyChar: Char = dict["character"]?.get(0) ?: return false
+
+    for (char in dict["password"].orEmpty()) {
+        if (char == policyChar)
+            occurrence++
+    }
+
+    return occurrence in lowerRange..upperRange
+}
+
+fun getMetrics(line: String): Map<String, String> {
+    val lineSplit = line.split(':')
+    val policy = lineSplit[0]
+    val password = lineSplit[1].split(' ')[1]
+
+    val policySplit = policy.split(' ')
+    val range = policySplit[0]
+    val character = policySplit[1]
+
+    val rangeSplit = range.split('-')
+    val rangeLower = rangeSplit[0]
+    val rangeUpper = rangeSplit[1]
+
+    return mapOf("first" to rangeLower, "second" to rangeUpper, "character" to character, "password" to password)
+}
